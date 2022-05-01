c6f21c1d756bd8f3f15da208147395722d10d901
Glitch (starter-react-template) <none>
2022-05-01 10:12:08 +0000

    🗻🏟 Checkpoint ./src/App.css:43125880/409 ./src/App.js:43125880/60

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
+      <div class="App">
       <header>
         <form id="todoform">
           <input type="text" placeholder="Enter Subject" />
           <input type="submit" value="Add" />
         </form>
       </header>
+      </div>
     );
   }
 }
