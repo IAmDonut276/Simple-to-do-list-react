2677a795cf8fd2a2c15a0848c0e73f317557f3aa
Glitch (starter-react-template) <none>
2022-05-01 11:22:15 +0000

    ✋🌜 Checkpoint ./src/App.js:43125880/1905

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        text: "",
+        key: "",
+      }
+    }
+    this.handleInput = this.handleInput.bind(this)
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+  }
   render() {
     return (
       <div class="App">
-      <header>
-        <form id="todoform">
-          <input type="text" placeholder="Enter Subject" />
-          <input type="submit" value="Add" />
-        </form>
-      </header>
+        <header>
+          <form id="todoform" onSubmit={this.addItem}>
+            <input
+              type="text"
+              placeholder="Enter Subject"
+              value={this.state.currentItem.text}
+              onChange={this.handleInput}
+            />
+            <input type="submit" value="Add" />
+          </form>
+        </header>
       </div>
     );
   }
